ac33c47367bb74404e9e622c8062d334d1bb44bf
Dao Le <lechanhdao240688@gmail.com>
2016-05-24 19:02:31 +0700

    temp commit

--- app/src/main/java/com/dreamdigitizers/megamelodies/views/classes/fragments/screens/ScreenSearch.java
+++ app/src/main/java/com/dreamdigitizers/megamelodies/views/classes/fragments/screens/ScreenSearch.java
@@ -9,6 +9,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.dreamdigitizers.androidbaselibrary.views.classes.fragments.screens.ScreenBase;
 import com.dreamdigitizers.megamelodies.R;
@@ -17,27 +19,14 @@ import com.dreamdigitizers.megamelodies.presenters.interfaces.IPresenterSearch;
 import com.dreamdigitizers.megamelodies.views.interfaces.IViewSearch;
 
 public class ScreenSearch extends ScreenBase<IPresenterSearch> implements IViewSearch {
-    protected MenuItem mActionSearch;
-    protected SearchView mSearchView;
+    private SearchView mSearchView;
+    private Spinner mSpnServers;
 
     @Override
     protected void createOptionsMenu(Menu pMenu, MenuInflater pInflater) {
         pInflater.inflate(R.menu.menu__search_screen, pMenu);
-        SearchManager searchManager = (SearchManager) this.getContext().getSystemService(Context.SEARCH_SERVICE);
-        this.mActionSearch = pMenu.findItem(R.id.option_menu_item__search);
-        this.mSearchView = (SearchView) this.mActionSearch.getActionView();
-        this.mSearchView.setSearchableInfo(searchManager.getSearchableInfo(this.getActivity().getComponentName()));
-        this.mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String pQuery) {
-                return ScreenSearch.this.onSearchViewQueryTextSubmitted(pQuery);
-            }
-
-            @Override
-            public boolean onQueryTextChange(String pNewText) {
-                return ScreenSearch.this.onSearchViewQueryTextChanged(pNewText);
-            }
-        });
+        this.setUpSearchViewMenuItem(pMenu);
+        this.setUpServerSpinnersMenuItem(pMenu);
     }
 
     @Override
@@ -69,6 +58,32 @@ public class ScreenSearch extends ScreenBase<IPresenterSearch> implements IViewS
     @Override
     protected int getTitle() {
         return R.string.title__search;
+    }
+
+    private void setUpSearchViewMenuItem(Menu pMenu) {
+        SearchManager searchManager = (SearchManager) this.getContext().getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchMenuItem = pMenu.findItem(R.id.option_menu_item__search);
+        this.mSearchView = (SearchView) searchMenuItem.getActionView();
+        this.mSearchView.setSearchableInfo(searchManager.getSearchableInfo(this.getActivity().getComponentName()));
+        this.mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String pQuery) {
+                return ScreenSearch.this.onSearchViewQueryTextSubmitted(pQuery);
+            }
+
+            @Override
+            public boolean onQueryTextChange(String pNewText) {
+                return ScreenSearch.this.onSearchViewQueryTextChanged(pNewText);
+            }
+        });
+    }
+
+    private void setUpServerSpinnersMenuItem(Menu pMenu) {
+        ArrayAdapter<CharSequence> listAdapter = ArrayAdapter.createFromResource(this.getContext(), R.array.servers, android.R.layout.simple_spinner_item);
+        listAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        MenuItem serversMenuItem = pMenu.findItem(R.id.option_menu_item__servers);
+        this.mSpnServers = (Spinner) serversMenuItem.getActionView();
+        this.mSpnServers.setAdapter(listAdapter);
     }
 
     private boolean onSearchViewQueryTextSubmitted(String pQuery) {
